92816a622ebc859240e24df1174bc0a3137b7d26
jardinthee <158405964+jardinthee@users.noreply.github.com>
2024-02-13 20:41:14 +0900

    Title: 최대공약수 구하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240213/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,8 @@
+n, m = tuple(map(int,input().split(' ')))
+
+def gong_finder(n,m):
+    g_list = []
+    for i in range(1,101):
+        if (n%i == 0) and (m%i ==0):
+            g_list.append(i)
+    print(g_list[-1])
