7eefd411bccce89d072a1d2ebfeca556a73a5ed5
jardinthee <158405964+jardinthee@users.noreply.github.com>
2024-02-15 13:51:02 +0900

    Title: 정수의 최솟값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240215/정수의 최솟값/minimum-value-of-an-integer.py
+++ 240215/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,20 +1,19 @@
-a,b,c= map(int,(input().split(' ')))
-alist = []
+a, b, c = map(int, input().split(' '))
 
 alist = [a, b, c]
 
-def cal(a,b,c):
+def cal(a, b, c):
     mi = []
     val = 1
     mi.append(a)
     for i in alist:
-       if val == 3:
-        break 
-       if alist[val] < mi[0]:
-        mi[0] = alist[val]
-       else:
-        pass
-       val += 1     
-    return(mi[0])
+        if val == 3:
+            break 
+        if alist[val] < mi[0]:
+            mi[0] = alist[val]
+        else:
+            pass
+        val += 1     
+    return mi[0]
 
-print(cal(a,b,c))
+print(cal(a, b, c))
