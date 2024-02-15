3633b9141e7953fa554a02f5fc5dbc521293bc17
jardinthee <158405964+jardinthee@users.noreply.github.com>
2024-02-15 17:05:07 +0900

    Title: 최소공배수 구하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,17 @@
+n,m = map(int,(input().split()))
+
+def max_gong(n,m):
+    a_list = []
+    val = 1
+    for i in range(1,101):
+        if i > n and i> m:
+            break
+        elif n%i == 0 and m%i ==0:
+            a_list.append(i)
+    for j in range(len(a_list)):
+        val*= a_list[j]
+        
+   
+    print(val)
+
+max_gong(n,m)
