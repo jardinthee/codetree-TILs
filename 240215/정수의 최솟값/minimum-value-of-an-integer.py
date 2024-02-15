5d3af45b7265309cbf17e63f3b1f820aa755d1f8
jardinthee <158405964+jardinthee@users.noreply.github.com>
2024-02-15 13:51:39 +0900

    Title: 정수의 최솟값, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/정수의 최솟값/minimum-value-of-an-integer.py
+++ 240215/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,19 +1,16 @@
-a, b, c = map(int, input().split(' '))
+# 변수 선언 및 입력:
+a, b, c = tuple(map(int, input().split()))
 
-alist = [a, b, c]
 
-def cal(a, b, c):
-    mi = []
-    val = 1
-    mi.append(a)
-    for i in alist:
-        if val == 3:
-            break 
-        if alist[val] < mi[0]:
-            mi[0] = alist[val]
-        else:
-            pass
-        val += 1     
-    return mi[0]
+# 최솟값을 반환하는 함수를 작성합니다.
+def get_min(a, b, c):
+    min_val = a
+    if min_val > b:
+        min_val = b
+    if min_val > c:
+        min_val = c
 
-print(cal(a, b, c))
+    return min_val
+
+
+print(get_min(a, b, c))
