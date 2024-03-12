5b7d8bbaa012197dd40fff16cfe08ebafe365217
jardinthee <158405964+jardinthee@users.noreply.github.com>
2024-03-12 20:22:29 +0900

    Title: palindrome 여부 판단하기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240312/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,12 +1,18 @@
-A = list(input())
+A = input()
 
 def is_pal(N):
+    list_a=[]
     for i in N[::-1]:
-        for j in N:
-            if i == j:
-                return True
+        list_a.append(i)
+    a = "".join(list_a)
+    if a == N:
+        return True
+    else:
+        return False
 
 if is_pal:
     print("Yes")
 else:
     print("No")
+
+is_pal(A)
